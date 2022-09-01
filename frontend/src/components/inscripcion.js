dee705d54a6fdfbf9d41d7896cb2347855ac4773
Dante Grizia SW <81974858+DanteGrSW@users.noreply.github.com>
2022-09-01 20:24:32 -0300

    dropdown de clases disponibles

--- frontend/src/components/inscripcion.js
+++ frontend/src/components/inscripcion.js
@@ -10,12 +10,16 @@ const CarsList = (props) => {
 
     const [carrerasDisponibles, setCarrerasDisponibles] = useState([]);
     const [clasesDisponibles, setClasesDisponibles] = useState([]);
+    const [claseSeleccionada, setClaseSeleccionada] = useState([]);
 
     useEffect(() => {
         retrieveCarreras();
     }, []);
 
-
+    const onChangesetSelectedClass = (e) => {
+        const clase = e.target.value;
+        setClaseSeleccionada(clase);
+      };
     /*
         En principio para anotarse se pediria
             Clase en la que se quiere correr
@@ -24,17 +28,18 @@ const CarsList = (props) => {
             fecha (determinada por el viernes mas proximo cargado en el sistema en la tabla Carreras)
 
         Para traer las clases disponibles, primero tenemos que recorrer la tabla de carreras
-        - al recorrer la tabla de carreras, buscamos todos los documentos que tengan fecha del proximo viernes mas cercano
-        - al tener todos esos documentos, podemos recorrerlos para obtener el ID de clase de cada uno de esos documentos, teniendo
+        DONE - al recorrer la tabla de carreras, buscamos todos los documentos que tengan fecha del proximo viernes mas cercano
+        DONE - al tener todos esos documentos, podemos recorrerlos para obtener el ID de clase de cada uno de esos documentos, teniendo
             asi entonces todas las clases disponibles para el viernes mas proximo
-        - teniendo todos los id de clase disponibles, podemos ofrecer el dropdown con los valores de clases que se pueden elegir
+        TODO ACA - teniendo todos los id de clase disponibles, podemos ofrecer el dropdown con los valores de clases que se pueden elegir
         - la fecha es dada por la data de lo obtenido en la tabla de carreras
-        - antes de habilitar la clase para ser seleccionada, se debe tambien validar que para esa carrera(fecha) haya cupos disponibles
+        DONE - antes de habilitar la clase para ser seleccionada, se debe tambien validar que para esa carrera(fecha) haya cupos disponibles
     */ 
     const retrieveCarreras = async () => {
-    await InscripcionDataService.getAll()
+    await InscripcionDataService.getAvailable()
         .then((response) => {
-        console.log("Data: ", response.data);
+            console.log("Data: ", response.data);
+            setClasesDisponibles(response.data.availableRaces);
         })
         .catch((e) => {
         console.log(e);
@@ -44,7 +49,18 @@ const CarsList = (props) => {
     if (cookies.get("_id")){
         return (
             <div class="container-fluid">
-                <p class="h1 text-center">Bienvenido a S4R</p>
+                <p class="h1 text-center">Inscripcion a Carrera</p>
+                <div className="input-group col-lg-4">
+                    <p class="h1 text-center">Clases disponibles para este viernes</p>
+                    <select onChange={onChangesetSelectedClass}>
+                    {clasesDisponibles.map(param => {
+                    console.log(param);
+                      return (
+                        <option value={param.carreraIdClase}> {param.carreraIdClase} </option>
+                      )
+                    })}
+                    </select>
+                  </div>
             </div>
         );
     
